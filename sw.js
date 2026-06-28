ad0b4ce0dc47b1314c9ca167672e14a8f6a8e475
comunicomstudio-source <comunicom.studio@gmail.com>
2026-06-28 08:34:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // y funcione aunque no haya conexión.
 
 // Sube este número cuando cambies los archivos cacheados (fuerza actualización).
-const VERSION = "gaston-v18";
+const VERSION = "gaston-v19";
 
 // Archivos que forman la cáscara de la app.
 const ARCHIVOS = [
